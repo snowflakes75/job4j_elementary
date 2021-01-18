42835c437ac8de5b776b42a1b9bf2b39b1f926fd
Mikle <gumenyukmichael@gmail.com>
2021-01-19 01:30:29 +0300

    6.0.1.1. Размер массива [#447961]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("Size of short[] " + ages.length);
         String[] surnames = new String[100500];
+        System.out.println("Size of String[] " + surnames.length);
         float[] prices = new float[40];
+        System.out.println("Size of float[] " + prices.length);
     }
 }
