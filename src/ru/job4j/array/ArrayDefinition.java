15afc3cfe45c81778970777847d456bab669b021
Mikle <gumenyukmichael@gmail.com>
2021-01-19 01:36:25 +0300

    6.0.2. Заполнение массива. [#447950]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Size of String[] " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Size of float[] " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Bob";
+        names[1] = "Alice";
+        names[2] = "Jhon";
+        names[3] = "Ricardo";
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
